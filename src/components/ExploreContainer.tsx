d4edc449f054dacb2f2453edadf940b5bc80b6a7
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-02-21 06:55:22 +0200

    fx

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,43 @@
 import './ExploreContainer.css';
 
+import React from 'react';
+import { IonSlides, IonSlide, IonContent,IonButton,IonIcon } from '@ionic/react';
+
 interface ContainerProps { }
+
+const slideOpts = {
+    initialSlide: 0,
+    speed: 400
+};
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+      <IonContent>
+          <IonSlides pager={true} options={slideOpts}>
+              <IonSlide>
+                  <div className="slide">
+                      <img src="./slides/slide-1.png"/>
+                      <h2>Добре дошъл!</h2>
+                      <p>Благодаря, че изтегли мобилното приложение за чат с Ванеса!</p>
+                  </div>
+              </IonSlide>
+              <IonSlide>
+                  <div className="slide">
+                      <img src="./slides/slide-2.png"/>
+                      <h2>Остават само няколко стъпки!</h2>
+                      <p><b>Регистрация</b> ще ти помогне да се сближиш с Ванеса по от близо!</p>
+
+                  </div>
+              </IonSlide>
+              <IonSlide>
+                  <div className="slide">
+                      <img src="./slides/slide-3.png"/>
+                      <h2>Готов си за това?</h2>
+                      <IonButton fill="clear">Регистрация <IonIcon slot="end" name="arrow-forward"></IonIcon></IonButton>
+                  </div>
+              </IonSlide>
+          </IonSlides>
+      </IonContent>
   );
 };
 
